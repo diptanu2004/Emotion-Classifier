6a5ca56004788125939af22e7c0a95010b828501
diptanu2004 <135000809+diptanu2004@users.noreply.github.com>
2025-06-26 13:39:16 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+import streamlit as st
+import tensorflow as tf
+import cv2
+categories = ['angry', 'disgusted', 'feared', 'happy', 'neutral', 'sad', 'surprised']
+def findFace(pathForImage):
+    image = cv2.imread(pathForImage)
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    haarCascadeFile = "haarcascade_frontalface_default.xml"
+    face = cv2.CascadeClassifier(haarCascadeFile)
+    faces = face.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        roiGray = gray[y:y+h, x:x+w]
+
+    return roiGray
+def prepareImageForModel(faceImage):
+     resized = cv2.resize(faceImage, (48,48), interpolation = cv2.INTER_AREA)
+     imgResult = np.expand_dims(resized, axis=0)
+     imgResult = imgResult/255.0
+     return imgResult
 @st.cache_resource
 def load_model():
     model = tf.keras.models.load_model('model.hdf5')
     return model
 model = load_model()
 st.write("Emotion Classifier")
-file = st.file_uploader("Please upload an image of a person", type=["jpg","png"])if file is None:
+file = st.file_uploader("Please upload an image of a person", type=["jpg","png"])
+if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
